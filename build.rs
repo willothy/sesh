5763de98dca19a4c17b1bea99a6ad1b5df75f1f8
Will Hopkins <willothyh@gmail.com>
2023-04-23 20:17:15 -0700

    docs: informative build error when protobuf not found

--- build.rs
+++ build.rs
@@ -1,8 +1,42 @@
+use sesh_cli::Cli;
 use std::path::PathBuf;
 
-use sesh_cli::Cli;
+const RED: &str = "\x1b[31m";
+const GREEN: &str = "\x1b[32m";
+const LIGHT_BLUE: &str = "\x1b[94m";
+
+const BG_DARK_GRAY: &str = "\x1b[100m";
+
+const RESET: &str = "\x1b[0m";
+
+const BOLD: &str = "\x1b[1m";
+const UNDERLINE: &str = "\x1b[4m";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Make sure protoc is installed
+    which::which("protoc").map_err(|_|
+        anyhow::anyhow!(
+        "{RED}Protobuf installation not found{RESET}.
+
+{BOLD}{GREEN}protoc{RESET} (the {BOLD}Protobuf{RESET} compiler) is required to build {GREEN}{BOLD}Sesh{RESET} from source.
+
+Please install it, and try again. Go to {UNDERLINE}{LIGHT_BLUE}https://grpc.io/docs/protoc-installation/{RESET} for instructions.
+    
+With apt on Ubuntu, you can install it with:
+    {BG_DARK_GRAY}\n
+    sudo apt install protobuf-compiler
+    {RESET}
+
+On Arch Linux, you can install it with:
+    {BG_DARK_GRAY}\n
+    sudo pacman -S protobuf
+    {RESET}
+
+On MacOS, you can install it with:
+    {BG_DARK_GRAY}\n
+    brew install protobuf
+    {RESET}
+"    ))?;
     let markdown: String = clap_markdown::help_markdown::<Cli>();
     let workspace_dir = std::env::var("CARGO_MANIFEST_DIR")?;
     std::fs::write(
